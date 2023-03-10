096ba5a3a92151cf14fdfa7520c921d806e14a50
LeninMatos <leninmatos@gmail.com>
2023-03-10 11:27:44 -0300

    Entendo o Event Loop e o Call Stack

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Olá, estou aqui");
+console.log("Mandando oi para o amigo");
+
+function mandarMensagem() {
+    console.log("Tudo bem?");
+    console.log("Vou te mandar um solicitação");
+    console.log("solicitação recebida")
+}
+
+mandarMensagem();
+
+console.log("Tchau Tchau!");
