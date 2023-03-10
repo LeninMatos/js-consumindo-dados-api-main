76f755946a3bd4520a0c8f148b567607f165007a
LeninMatos <leninmatos@gmail.com>
2023-03-10 16:48:58 -0300

    usando o try e o async

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/') //requisita informações do API
-    .then(resposta => resposta.json()) //Transforma as informaçoes
-    .then(r => { //sendo a entrada válida
-        if (r.erro) { //se for uma entrada de CEP que não existe
-            throw Error('Esse CEP não existe');
-        } else //se for uma entrada que existe mostra o resultado "errado" ou não
-            console.log(r); 
-    })
-    .catch(erro => console.log(erro)) //mostra o erro
-    .finally(mensagem => console.log('Processamento concluído!')); //conclui o código
-
-console.log(consultaCep);
+async function buscaEndereco() {
+    try {
+        var consultaCep = await fetch('https://viacep.com.br/ws/01001250/json/');
+        var consultaCEPConvertida = await consultaCep.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP não existente');
+        }
+        console.log(consultaCEPConvertida);
+    } catch (erro) {
+        console.log(erro)
+    }
+}
+buscaEndereco();
