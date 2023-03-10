2c2bc5b605bc9cc4fd06f8f469d6b95e20df88f1
LeninMatos <leninmatos@gmail.com>
2023-03-10 16:23:12 -0300

    Usando o Cacht para o erro e Finally pra encerrar

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
-    .then(resposta => resposta.json())
-    .then(r => (console.log(r)));
+var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/') //requisita informações do API
+    .then(resposta => resposta.json()) //Transforma as informaçoes
+    .then(r => { //sendo a entrada válida
+        if (r.erro) { //se for uma entrada de CEP que não existe
+            throw Error('Esse CEP não existe');
+        } else //se for uma entrada que existe mostra o resultado "errado" ou não
+            console.log(r); 
+    })
+    .catch(erro => console.log(erro)) //mostra o erro
+    .finally(mensagem => console.log('Processamento concluído!')); //conclui o código
 
 console.log(consultaCep);
