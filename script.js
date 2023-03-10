509e5a2346517b63074d1758f04d95dd5589b447
LeninMatos <leninmatos@gmail.com>
2023-03-10 15:54:38 -0300

    usando o método fetch para usar o then e o json

--- script.js
+++ script.js
@@ -1,35 +1,5 @@
-var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/');
+var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
+    .then(resposta => resposta.json())
+    .then(r => (console.log(r)));
 
 console.log(consultaCep);
-
-/* Event Looo + Call Stack*/ 
-/* console.log("Mandando oi para o amigo");
-
-function mandarMensagem() {
-    console.log("Tudo bem?");
-    console.log("Vou te mandar um solicitação");
-    console.log("solicitação recebida")
-}
-
-mandarMensagem();
-
-console.log("Tchau Tchau!"); 
-
-*/ 
-
-/* Task Queue*/
-
-/*console.log("Mandando oi para o amigo");
-
-function mandarMensagem() {
-    console.log("Tudo bem?");
-    console.log("Vou te mandar um solicitação");
-    console.log("solicitação recebida")
-}
-
-setTimeout(mandarMensagem, 5000); /* aqui temos um callback - funções como parametros de funções - requisições são mais usadas.
-
-console.log("Tchau Tchau!"); */
-
-
-
